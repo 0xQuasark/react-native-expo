71b5d90caa801d3495ff1ffd85fcf50e5b695593
Quasark <q@quasark.xyz>
2023-11-13 13:01:16 -0800

    location added

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button,  SafeAreaView, Linking } from 'react-native';
+
+import * as Contacts from 'expo-contacts';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [locationData, setLocationData] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [locationText, setLocationText] = useState('Waiting..');
+
+  const [contactData, setContactData] = useState([]);
+
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+
+    let location  = await Location.getCurrentPositionAsync({});
+    setLocationData(location);
+    setLocationText(`Lat: ${locationData.coords.latitude}, Long: ${locationData.coords.longitude}`);
+
+    console.log('Get location', locationData);
+  }
+
+  const getContacts = async () => {
+    let { status } = await Contacts.requestPermissionsAsync(); // ask for permission
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync();
+      setContactData(data);
+    }
+  }
+
+  const call = (contact) => {
+    let userName = contact.name;
+    let phoneNumber = contact.phoneNumbers[0].number;
+    let link = 'tel:' + phoneNumber; // protocol + URL equiv (like http://)
+    Linking.canOpenURL(link)
+      .then(isSupported=> Linking.openURL(link))
+      .catch((e) => {
+        console.log('error',e);
+      });
+
+    console.log(contact.name, );
+  }
+
+  useEffect(() => {
+    console.log('App is loaded');
+    getContacts();
+    getLocation();
+  }, []);
+
+  // console.log('here i is', locationData);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text>Contacts thinge</Text>
+        <FlatList 
+          data={contactData}  // a list of data objects to do something with
+          keyExtractor={contact => contact.id}     // a function that returns a unique key for each item
+          renderItem={({ item }) =>  
+            <Button 
+              title={item.name} 
+              onPress={() => call(item)} 
+            />
+          }
+        />
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.footer}>
+        <Text>Location Data Received:</Text>
+        <Text style={styles.locationInfo}>{locationText}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
+  },
+  footer: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: 'blue',
+  },
+  locationInfo:{
+    flex: 1,
+    color: 'purple',
+  },
+  container: {
+    flex: 2,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 2,
+    borderColor: 'purple',
   },
+  footerText: {
+    color: 'red',
+  }
 });
---
+++ README.md
@@ -0,0 +1,21 @@
+# Lab41 - Proof of Life iOS on React Native
+
+## Introduction
+
+This README provides instructions on how to install and use the application. Please follow the steps below to ensure a smooth setup and execution.
+
+## Installation
+
+1. Run the command `npx expo install expo-contacts` in your terminal.
+2. Run the command `npx expo install expo-location` in your terminal.
+3. Run the command `npm start` in your terminal.
+4. Choose the iOS version you want to use.
+5. Make sure you have the latest version of Xcode installed.
+6. Ensure your Simulator is updated and has iOS installed.
+
+## Troubleshooting
+
+If you encounter any issues, please refer to the official Expo documentation or raise an issue in this repository.
+[docs.expo.dev](https://docs.expo.dev/)
+
+
